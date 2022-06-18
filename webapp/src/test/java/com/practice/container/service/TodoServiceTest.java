855e6c6b4cf4118ce0f5f569d926d64099161987
be834194 <type.x.supernova.555@gmail.com>
2022-06-18 13:19:53 +0900

    ファイル微修正

--- webapp/src/test/java/com/practice/container/service/TodoServiceTest.java
+++ webapp/src/test/java/com/practice/container/service/TodoServiceTest.java
@@ -9,16 +9,12 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-//import org.springframework.test.context.junit4.SpringRunner;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import com.practice.container.entity.Todo;
 import com.practice.container.repository.TodoRepository;
